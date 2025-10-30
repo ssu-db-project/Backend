05997660d44566cd94947dfae88362644de7e2c1
WINDOWLSH\ndamg <ndamgom@naver.com>
2025-10-30 13:31:05 +0900

    initial set up

---
+++ src/main/java/com/ssu/DB_Project/DbProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ssu.DB_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbProjectApplication.class, args);
+	}
+
+}
