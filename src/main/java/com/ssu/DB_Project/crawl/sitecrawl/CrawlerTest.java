d3fc285dac4151bbca19436a68b8735a1e9dd6a7
backtrap <b354b.36750@gmail.com>
2025-11-23 15:56:26 +0900

    슈패스 크롤링 구현

---
+++ src/main/java/com/ssu/DB_Project/crawl/sitecrawl/CrawlerTest.java
@@ -0,0 +1,23 @@
+package com.ssu.DB_Project.crawl.sitecrawl;
+
+import com.ssu.DB_Project.crawl.sitecrawl.SsuPath;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CrawlerTest implements CommandLineRunner {
+
+    @Autowired
+    private SsuPath ssuPath; // 우리가 만든 크롤러 주입
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("=========== [TEST] 크롤링 테스트 시작 ===========");
+
+        // 여기서 크롤링 메서드 강제 실행
+        ssuPath.crawl();
+
+        System.out.println("=========== [TEST] 크롤링 테스트 종료 ===========");
+    }
+}
--- src/main/java/com/ssu/DB_Project/program/repository/ProgramRepository.java
+++ src/main/java/com/ssu/DB_Project/program/repository/ProgramRepository.java
@@ -4,5 +4,5 @@ import com.ssu.DB_Project.program.domain.Program;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProgramRepository extends JpaRepository<Program, String> {
-
+    boolean existsByOriginalUrl(String originalUrl);
 }
